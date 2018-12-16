f57e86f5d8038e2660e74d76162e15931a8dc053
Unknown <e.karamihov@student.fontys.nl>
2018-12-16 22:20:01 +0100

    Test Exception

--- tci-project/src/test/java/WebCrawlerTest.java
+++ tci-project/src/test/java/WebCrawlerTest.java
@@ -1,6 +1,13 @@
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class WebCrawlerTest {
 
-
-
-
+    @Test(expected = MalformedURLException.class)
+    public void testWebPageCraw() throws MalformedURLException {
+        WebCrawler crawler = null;
+        crawler = new WebCrawler(new URL("156"));
+    }
 }
